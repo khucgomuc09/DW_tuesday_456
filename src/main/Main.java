e5fba5211462bb5ae1bde99a0479c55a510804ce
LangDuyen1233 <myduyen123150899@gmail.com>
2020-08-08 11:48:47 +0700

    merge code

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,22 +9,13 @@ import staging.ExtractFile;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-		int id = 1;
-=======
 		int id = 0;
->>>>>>> a904260f942f7b5ccea84d992df994921322ccf9
 		for (int i = 0; i < args.length; i++) {
 			id = Integer.parseInt(args[i]);
 			System.out.println(args[i]);
 		}
 		DownloadFileToLocal.downloadFile(id + "");
-//		try {
-//
 		ExtractFile.staging(id);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
 
 	}
 
